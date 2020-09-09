817ab3db1a2d0cc2175e9bf1538285e3899acb91
lvndry <monga.4landry@gmail.com>
2020-09-09 16:36:30 +0200

    console.log statement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'flickrSearchEngine';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const love = String.fromCodePoint(0x1f60d);
+    console.log(`Made with ${love} by Landry Monga and William Ngbama`);
+  }
 }
