6253811a2f9d013f79025fc1749fc2e55466d521
PellYm785 <williamliya@hotmail.fr>
2020-09-11 16:49:16 +0200

    add primng animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
+import { DropdownModule } from 'primeng/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,17 +13,19 @@ import { SearchComponent } from './search/search.component';
 import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchComponent, SliderComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    ButtonModule,
-    CarouselModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, SearchComponent, SliderComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ButtonModule,
+        CarouselModule,
+        DropdownModule,
+        BrowserAnimationsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
