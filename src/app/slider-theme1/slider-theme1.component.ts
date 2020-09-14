3d374980b2b16607227cc3dac9063619f3c746ef
PellYm785 <williamliya@hotmail.fr>
2020-09-14 12:36:25 +0200

    create slider theme1

---
+++ src/app/slider-theme1/slider-theme1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-theme1',
+  templateUrl: './slider-theme1.component.html',
+  styleUrls: ['./slider-theme1.component.css']
+})
+export class SliderTheme1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
