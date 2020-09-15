09c38278f3cafcbec1d034b11b761d458075e612
lvndry <monga.4landry@gmail.com>
2020-09-15 15:26:17 +0200

    all tests pass

--- src/app/image/image.component.spec.ts
+++ src/app/image/image.component.spec.ts
@@ -1,25 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImageComponent } from './image.component';
+import { mockPhoto } from 'src/app/models';
 
 describe('ImageComponent', () => {
-  let component: ImageComponent;
-  let fixture: ComponentFixture<ImageComponent>;
+    let component: ImageComponent;
+    let fixture: ComponentFixture<ImageComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ImageComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ImageComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ImageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ImageComponent);
+        component = fixture.componentInstance;
+        component.image = mockPhoto;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should return correct time', () => {
+        const timestamp = 1600054694;
+        expect(component.toLocalDate(timestamp)).toBe('14/09/2020');
+    });
 });
--- src/app/models.ts
+++ src/app/models.ts
@@ -11,3 +11,17 @@ export interface Photo {
     longitutde: string;
     latitude: string;
 }
+
+export const mockPhoto: Photo = {
+    id: '3453',
+    title: 'Test photo',
+    farm: 1,
+    secret: 3334,
+    server: 243,
+    description: { _content: '' },
+    dateupload: '5425454',
+    views: '33',
+    ownername: 'test',
+    longitutde: '44.55',
+    latitude: '-22.045',
+};
